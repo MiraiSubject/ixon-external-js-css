4ba3a8d0de6629261fa55b5e544849ac4247e6ca
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2022-12-29 19:57:20 +0100

    Add reproduction files

---
+++ components/svelte-test/svelte-test.svelte
@@ -0,0 +1,18 @@
+<!-- https://sveltestrap.js.org/?path=/story/components--get-started -->
+
+<script>
+  import { Styles, Button, Modal } from 'sveltestrap';
+  let isOpen = false;
+  const toggle = () => (isOpen = !isOpen);
+</script>
+
+<Styles />
+<Button color="primary" on:click={toggle}>Hello World!</Button>
+<Modal body {isOpen} {toggle} header="Hello World!">
+  <p>There's a song that we're singing. Come on</p>
+  <img
+    src="https://i.ytimg.com/vi/NUJIRujygvY/hqdefault.jpg"
+    alt="Come on Get Happy"
+    class="img-fluid"
+  />
+</Modal>
---
+++ components/test/test.vue
@@ -0,0 +1,21 @@
+<!-- https://stackoverflow.com/questions/65547199/using-bootstrap-5-with-vue-3 -->
+
+<template>
+  <main>
+    <button class="btn btn-primary" data-bs-target="#collapseTarget" data-bs-toggle="collapse">
+      Bootstrap collapse
+    </button>
+    <div class="collapse py-2" id="collapseTarget">
+      This is the toggle-able content!
+    </div>
+  </main>
+</template>
+
+<script>
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle"
+export default {
+  name: 'test',
+  props: ['context'],
+};
+</script>
